c7677e1754bbfc7b0ff3c518ff153f70da16c4ee
eschultz-ensae <emilien.schultz@ensae.fr>
2026-06-15 09:07:15 +0200

    update

--- deploy_phase2.sh
+++ deploy_phase2.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Patch the problem of routes
+
+set -e
+# Find the interface whose default route uses a private (10.x / 172.16-31 / 192.168) source
+IFACE=$(ip -o -4 route show to default | awk '$9 ~ /^(10\.|172\.(1[6-9]|2[0-9]|3[01])\.|192\.168\.)/ {print $5; exit}')
+IFACE=${IFACE:-ens4}
+cat > /etc/netplan/99-ovh-docker-fix.yaml <<EOF
+network:
+  version: 2
+  ethernets:
+    ${IFACE}:
+      dhcp4: true
+      dhcp4-overrides:
+        use-routes: false
+EOF
+chmod 600 /etc/netplan/99-ovh-docker-fix.yaml
+echo 'network: {config: disabled}' > /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg
+netplan apply
+
 cd "$HOME/activetigger/docker"
 
 # Launch modes
